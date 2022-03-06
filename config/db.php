94c59b45c4d0895cdb67f2002f089833fc5e6ccc
denis <denj88@mail.ru>
2022-03-06 17:11:58 +0300

    init app

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=booking_rooms_db',
+    'username' => 'booking_rooms',
+    'password' => 'booking_rooms',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
